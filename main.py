e5763fb643b818af69e911b9afe5b8598b9b9d2f
Ryan Stewart <ryanstewartalex@gmail.com>
2022-11-04 04:03:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from os import path
 from PyQt6.QtWidgets import (QApplication,
                              QWidget,
                              QHBoxLayout,
@@ -28,13 +29,13 @@ class MainWindow(QWidget):
         self.setWindowIcon(QIcon("logo.png"))
         self.setStyleSheet("background-color: #24252b; color: white;")
 
-        self.location_edit = None
-        self.name_edit = None
-        self.call_sign_edit = None
-        self.mode_edit = None
-        self.frequency_edit = None
         self.date_edit = None
         self.time_edit = None
+        self.call_sign_edit = None
+        self.freq_edit = None
+        self.mode_edit = None
+        self.name_edit = None
+        self.location_edit = None
 
         self.col_date = None
         self.col_time = None
@@ -44,18 +45,6 @@ class MainWindow(QWidget):
         self.col_name = None
         self.col_location = None
 
-        self.right_vbox = None
-        self.scroll_area = None
-
-        font_id = QFontDatabase.addApplicationFont("VT323-Regular.ttf")
-        if font_id < 0:
-            print("ERROR LOADING FONT")
-        families = QFontDatabase.applicationFontFamilies(font_id)
-        self.button_font = QFont(families[0], 20)
-        self.header_font = QFont(families[0], 20)
-        self.label_font = QFont(families[0], 20)
-
-        self.entryList = []
         self.init_ui()
 
 
@@ -78,48 +67,35 @@ class MainWindow(QWidget):
         self.time_edit = QTimeEdit()
         self.time_edit.setTime(QDateTime.currentDateTime().time())
 
-        self.frequency_edit = QLineEdit()
+        self.freq_edit = QLineEdit()
         self.mode_edit = QLineEdit()
         self.call_sign_edit = QLineEdit()
         self.name_edit = QLineEdit()
         self.location_edit = QLineEdit()
 
-        label_style = "color: #ff885e;"
+        font_id = QFontDatabase.addApplicationFont(path.abspath("VT323-Regular.ttf"))
+        if font_id < 0:
+            print("ERROR LOADING FONT")
+        families = QFontDatabase.applicationFontFamilies(font_id)
+        button_font = QFont(families[0], 20)
+        label_font = QFont(families[0], 20)
 
-        date_label = QLabel("Date:")
-        date_label.setFont(self.label_font)
-        date_label.setStyleSheet(label_style)
-        left_layout_form.addRow(date_label, self.date_edit)
+        labels = {
+            "Date:": self.date_edit,
+            "Time:": self.time_edit,
+            "Call Sign:": self.call_sign_edit,
+            "Frequency:": self.freq_edit,
+            "Mode:": self.mode_edit,
+            "Name:": self.name_edit,
+            "Location": self.location_edit
+        }
 
-        time_label = QLabel("Time:")
-        time_label.setFont(self.label_font)
-        time_label.setStyleSheet(label_style)
-        left_layout_form.addRow(time_label, self.time_edit)
+        for label_text in labels:
+            label = QLabel(label_text)
+            label.setFont(label_font)
+            label.setStyleSheet("color: #ff885e;")
+            left_layout_form.addRow(label, labels[label_text])
 
-        call_sign_label = QLabel("Call Sign:")
-        call_sign_label.setFont(self.label_font)
-        call_sign_label.setStyleSheet(label_style)
-        left_layout_form.addRow(call_sign_label, self.call_sign_edit)
-
-        freq_label = QLabel("Frequency:")
-        freq_label.setFont(self.label_font)
-        freq_label.setStyleSheet(label_style)
-        left_layout_form.addRow(freq_label, self.frequency_edit)
-
-        mode_label = QLabel("Mode:")
-        mode_label.setFont(self.label_font)
-        mode_label.setStyleSheet(label_style)
-        left_layout_form.addRow(mode_label, self.mode_edit)
-
-        name_label = QLabel("Name:")
-        name_label.setFont(self.label_font)
-        name_label.setStyleSheet(label_style)
-        left_layout_form.addRow(name_label, self.name_edit)
-
-        loc_label = QLabel("Location:")
-        loc_label.setFont(self.label_font)
-        loc_label.setStyleSheet(label_style)
-        left_layout_form.addRow(loc_label, self.location_edit)
 
         left_layout.addLayout(left_layout_form)
 
@@ -127,7 +103,7 @@ class MainWindow(QWidget):
 
         btn_add = QPushButton("ADD ENTRY")
         btn_add.setStyleSheet(btn_style)
-        btn_add.setFont(self.button_font)
+        btn_add.setFont(button_font)
         left_layout.addWidget(btn_add)
         btn_add.clicked.connect(self.on_add_entry)
 
@@ -137,43 +113,28 @@ class MainWindow(QWidget):
         # Right section
         right_scroll_area = QScrollArea()
         right_widget = QWidget()
-        self.right_vbox = QVBoxLayout()
-        self.right_vbox.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignHCenter)
-        self.right_vbox.setContentsMargins(10, 10, 10, 10)
+        right_vbox = QVBoxLayout()
+        right_vbox.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignHCenter)
+        right_vbox.setContentsMargins(10, 10, 10, 10)
+
+        self.col_date = QVBoxLayout()
+        self.col_time = QVBoxLayout()
+        self.col_freq = QVBoxLayout()
+        self.col_mode = QVBoxLayout()
+        self.col_call_sign = QVBoxLayout()
+        self.col_name = QVBoxLayout()
+        self.col_location = QVBoxLayout()
 
         right_hbox = QHBoxLayout()
         column_margins = [10, 0, 10, 0]
+        columns = [self.col_date, self.col_time, self.col_call_sign, self.col_freq, self.col_mode, self.col_name, self.col_location]
 
-        self.col_date = QVBoxLayout()
-        self.col_date.setContentsMargins(*column_margins)
-        right_hbox.addLayout(self.col_date)
+        for column in columns:
+            column.setContentsMargins(*column_margins)
+            right_hbox.addLayout(column)
 
-        self.col_time = QVBoxLayout()
-        self.col_time.setContentsMargins(*column_margins)
-        right_hbox.addLayout(self.col_time)
-
-        self.col_freq = QVBoxLayout()
-        self.col_freq.setContentsMargins(*column_margins)
-        right_hbox.addLayout(self.col_freq)
-
-        self.col_mode = QVBoxLayout()
-        self.col_mode.setContentsMargins(*column_margins)
-        right_hbox.addLayout(self.col_mode)
-
-        self.col_call_sign = QVBoxLayout()
-        self.col_call_sign.setContentsMargins(*column_margins)
-        right_hbox.addLayout(self.col_call_sign)
-
-        self.col_name = QVBoxLayout()
-        self.col_name.setContentsMargins(*column_margins)
-        right_hbox.addLayout(self.col_name)
-
-        self.col_location = QVBoxLayout()
-        self.col_location.setContentsMargins(*column_margins)
-        right_hbox.addLayout(self.col_location)
-
-        self.right_vbox.addLayout(right_hbox)
-        right_widget.setLayout(self.right_vbox)
+        right_vbox.addLayout(right_hbox)
+        right_widget.setLayout(right_vbox)
 
         right_scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
         right_scroll_area.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
@@ -192,7 +153,7 @@ class MainWindow(QWidget):
     def on_add_entry(self):
         date = self.date_edit.date().toString("yyyy-MM-dd")
         t = self.time_edit.time().toString("hh:mm")
-        freq = self.frequency_edit.text()
+        freq = self.freq_edit.text()
         mode = self.mode_edit.text()
         call_sign = self.call_sign_edit.text()
         name = self.name_edit.text()
@@ -201,29 +162,14 @@ class MainWindow(QWidget):
         values = []
         for value in [date, t, freq, mode, call_sign, name, location]:
             if not value:
-                value = "N/A"
+                value = "-"
             values.append(value)
 
-        self.col_date.addWidget(QLabel(values[0]))
-        self.col_time.addWidget(QLabel(values[1]))
-        self.col_freq.addWidget(QLabel(values[2]))
-        self.col_mode.addWidget(QLabel(values[3]))
-        self.col_call_sign.addWidget(QLabel(values[4]))
-        self.col_name.addWidget(QLabel(values[5]))
-        self.col_location.addWidget(QLabel(values[6]))
-
-        # values =
-        # final_string = ""
-        # for i, value in enumerate(values):
-        #     if not value:
-        #         value = "N/A"
-        #
-        #     final_string += value
-        #
-        #     if i is not len(values):
-        #         final_string += "    "
-        #
-        # self.right_vbox.addWidget(QLabel(final_string))
+        columns = [self.col_date, self.col_time, self.col_freq, self.col_mode, self.col_call_sign, self.col_name, self.col_location]
+        for i, column in enumerate(columns):
+            label = QLabel(values[i])
+            label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+            column.addWidget(label)
 
 
 
