7431441de94aaccce0d8ab43fc9ba6cea36c25f7
Ryan Stewart <ryanstewartalex@gmail.com>
2022-11-08 12:14:14 -0600

    Begin Adding Stored  Config

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sys
 import os
+import json
 from PySide6.QtCore import QDateTime, Qt
 from PySide6.QtWidgets import (QApplication, QMainWindow, QDateEdit,
                                QHBoxLayout, QVBoxLayout, QLabel,
                                QTimeEdit, QComboBox, QLineEdit,
                                QWidget, QSizePolicy, QTableWidget,
                                QPushButton, QTableWidgetItem, QFormLayout,
-                               QHeaderView)
+                               QHeaderView, QFileDialog)
 from PySide6.QtGui import QIcon
 from qt_material import apply_stylesheet
 
@@ -27,7 +28,9 @@ class MainWindow(QMainWindow):
                       "DominoEX", "FSQ", "IFKP", "MFSK",
                       "MT63", "OFDM", "Olivia", "THOR",
                       "Throb", "WEFAX", "Navtex/SitorB"]
-        self.num_entries = 0
+
+        self.entries = []
+
         self.h_layout = None
         self.table = None
 
@@ -43,14 +46,12 @@ class MainWindow(QMainWindow):
         self.init_left_ui()
         self.init_right_ui()
 
-
     def init_ui(self):
         main_widget = QWidget()
         self.setCentralWidget(main_widget)
 
         self.h_layout = QHBoxLayout()
         main_widget.setLayout(self.h_layout)
-
 
     def init_left_ui(self):
         vert_section = QVBoxLayout()
@@ -105,12 +106,21 @@ class MainWindow(QMainWindow):
         btn_add.setSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
         btn_add.clicked.connect(self.on_add_button_clicked)
 
+        btn_save = QPushButton("SAVE")
+        btn_save.setSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
+        btn_save.clicked.connect(self.save_data)
+        btn_row.addWidget(btn_save)
+
+        btn_load = QPushButton("LOAD")
+        btn_load.setSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
+        btn_load.clicked.connect(self.load_data)
+        btn_row.addWidget(btn_load)
+
         btn_row.addWidget(btn_add)
         vert_section.addLayout(btn_row)
 
-
     def init_right_ui(self):
-        self.table = QTableWidget(self.num_entries, len(self.labels) + 1)
+        self.table = QTableWidget(len(self.entries), len(self.labels) + 1)
         self.table.setMinimumWidth(800)
 
         self.table.horizontalHeader().setDefaultAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -118,33 +128,40 @@ class MainWindow(QMainWindow):
         self.table.setHorizontalHeaderLabels(self.labels + ["DEL:"])
         self.table.verticalHeader().setVisible(False)
 
+        for i, entry in enumerate(self.entries):
+            for j, entry_part in enumerate(entry):
+                self.table.setItem(i, j, QTableWidgetItem(entry_part))
+
         self.h_layout.addWidget(self.table)
 
-
     def on_add_button_clicked(self):
-        values = [self.date_input.date().toString("yyyy-MM-dd"),
-                  self.time_input.time().toString("hh:mm"),
-                  self.call_sign_input.text(),
-                  self.frequency_input.text(),
-                  self.mode_input.currentText(),
-                  self.name_input.text(),
-                  self.location_input.text()]
+        input_values = [self.date_input.date().toString("yyyy-MM-dd"),
+                        self.time_input.time().toString("hh:mm"),
+                        self.call_sign_input.text(),
+                        self.frequency_input.text(),
+                        self.mode_input.currentText(),
+                        self.name_input.text(),
+                        self.location_input.text()]
+
+        values = []
 
         self.table.setRowCount(self.table.rowCount() + 1)
 
-        for i, value in enumerate(values):
+        for i, value in enumerate(input_values):
             if not value:
                 value = "/"
 
+            values.append(value)
+
             table_item = QTableWidgetItem(value)
-            self.table.setItem(self.num_entries, i, table_item)
+            self.table.setItem(len(self.entries), i, table_item)
             table_item.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.entries += [values]
 
         del_button = QPushButton("DEL")
         # del_button.clicked.connect(lambda: self.on_delete_button_clicked(del_index))
-        self.table.setCellWidget(self.num_entries, len(values), del_button)
-
-        self.num_entries += 1
+        self.table.setCellWidget(len(self.entries), len(values), del_button)
 
         current_date = QDateTime.currentDateTime().date()
         self.date_input.setMaximumDate(current_date)
@@ -160,11 +177,24 @@ class MainWindow(QMainWindow):
         self.name_input.clear()
         self.location_input.clear()
 
-
     def on_delete_button_clicked(self, index):
         print(index)
         self.table.removeRow(index)
-        self.num_entries -= 1
+
+    def load_data(self):
+        with open("entries.json", "r") as file_read:
+            json_data = json.load(file_read)
+
+        self.entries = json_data["entries"]
+        print(self.entries)
+
+    def save_data(self):
+        json_data = {"entries": []}
+        for entry in self.entries:
+            json_data["entries"] += [entry]
+
+        with open("entries.json", "w") as file_write:
+            json.dump(json_data, file_write)
 
 
 if __name__ == "__main__":
